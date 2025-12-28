14e28d564a21cc62700815ecba47123113690ae7
bastien baranoff <37385191+bbaranoff@users.noreply.github.com>
2025-12-28 14:08:19 +0100

    Add files via upload

---
+++ osmo-all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cmd="${1:-status}" 
+set -ex
+systemctl daemon-reload
+systemctl $cmd osmo-hlr \
+               osmo-msc \
+               osmo-mgw \
+               osmo-stp \
+               osmo-bsc \
+               osmo-ggsn \
+               osmo-sgsn \
+               osmo-sip-connector \
+               osmo-bts \
+               osmo-pcu
