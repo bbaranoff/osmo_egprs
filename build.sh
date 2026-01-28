f9a74c6c3660a142ed1b22d6d2831aa0dd116f0f
nirvana <bbaranoff>
2026-01-28 21:45:26 +0100

    commit

--- build.sh
+++ build.sh
@@ -14,8 +14,7 @@ if ! command -v docker &> /dev/null; then
     apt-get update
     apt-get install -y ca-certificates curl gnupg lsb-release
     mkdir -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
+y
     apt-get update
     apt-get install -y docker.io containerd.io
 fi
