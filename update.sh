a7bb382e2f9363493547e78d69b6cba9461b1183
bastien baranoff <37385191+bbaranoff@users.noreply.github.com>
2026-06-18 17:37:39 +0200

    Update keyboard setup and clone new repository

--- update.sh
+++ update.sh
@@ -1,6 +1,5 @@
 awk '1; /Disclaimer/{print "echo \"Hello !!!\""; exit}' /etc/profile.d/01-keyboard-setup.sh > /tmp/kb.new
 cat /tmp/kb.new > /etc/profile.d/01-keyboard-setup.sh
-
 sed -i -e 's/a5 1/a5 0/g' /etc/osmocom/osmo-*sc.cfg
 apt update && apt install git -y
 rm -r /opt/osmo-egprs-web
