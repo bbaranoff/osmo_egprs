49ad2049bab219adfdf986cfcd370c4be41b7ffd
bastien baranoff <37385191+bbaranoff@users.noreply.github.com>
2026-06-18 16:28:59 +0200

    Modify update.sh to install git and clone repos
    Update package and clone repositories for osmo-egprs-web.

--- update.sh
+++ update.sh
@@ -1,2 +1,8 @@
 echo hello
 sed -i -e 's/a5 1/a5 0/g' /etc/osmocom/osmo-*sc.cfg
+apt update && apt install git -y
+rm -r /opt/osmo-egprs-web
+git clone https://github.com/bbaranoff/osmo-egprs-web /opt/osmo-egprs-web
+
+rm -r /opt/GSM/osmo_egprs
+git clone https://github.com/bbaranoff/osmo-egprs-web /opt/GSM/osmo_egprs
