e606227ef26f79760372d26683c962a3780c18e6
bastien baranoff <37385191+bbaranoff@users.noreply.github.com>
2026-06-18 16:47:40 +0200

    Refactor update.sh for system setup and repository cloning
    Updated update.sh to modify keyboard setup and clone repositories.

--- update.sh
+++ update.sh
@@ -1,4 +1,11 @@
 echo hello
+
+sed -i.bak '\#^touch /var/lib/osmo-kb-done#,$c\
+touch /var/lib/osmo-kb-done\
+\
+echo -e "Hello From Bastien !"
+' /etc/profile.d/01-keyboard-setup.sh
+
 sed -i -e 's/a5 1/a5 0/g' /etc/osmocom/osmo-*sc.cfg
 apt update && apt install git -y
 rm -r /opt/osmo-egprs-web
@@ -6,8 +13,3 @@ git clone https://github.com/bbaranoff/osmo-egprs-web /opt/osmo-egprs-web
 
 rm -r /opt/GSM/osmo_egprs
 git clone https://github.com/bbaranoff/osmo_egprs /opt/GSM/osmo_egprs
-sed -i.bak '\#^touch /var/lib/osmo-kb-done#,$c\
-touch /var/lib/osmo-kb-done\
-\
-echo -e "Hello From Bastien !"
-' /etc/profile.d/01-keyboard-setup.sh
