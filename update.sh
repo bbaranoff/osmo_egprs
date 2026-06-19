2a942daa919e657c587a44967bf8158c14f3f881
bastien baranoff <37385191+bbaranoff@users.noreply.github.com>
2026-06-19 23:20:48 +0200

    Refactor git clone and add checkout for osmo_egprs
    Updated git clone commands to include checkout for osmo_egprs.

--- update.sh
+++ update.sh
@@ -25,7 +25,7 @@ cat /tmp/kb.new > /etc/profile.d/01-keyboard-setup.sh
 
 apt update && apt install git -y
 rm -r /opt/osmo-egprs-web
-git clone https://github.com/bbaranoff/osmo-egprs-web /opt/osmo-egprs-web
-
+git clone https://github.com/bbaranoff/osmo-egprs-web /opt/osmo-egprs-web 
 rm -r /opt/GSM/osmo_egprs
-git clone https://github.com/bbaranoff/osmo_egprs /opt/GSM/osmo_egprs
+git clone https://github.com/bbaranoff/osmo_egprs /opt/GSM/osmo_egprs && cd /opt/GSM/osmo_egprs && git checkout test
+
